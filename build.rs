b452a9eb0222e4a01f737a41b2d82ca90ecf7562
Marco A L Barbosa <malbarbo@gmail.com>
2024-10-15 11:15:14 -0300

    Use bigint version of gleam and stdlib

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use std::{env, path::PathBuf, process::Command};
 
 pub fn main() {
+    println!("cargo::rerun-if-changed=build.rs");
+
     let outdir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let stdlib = &outdir.join("gleam-stdlib");
     let tar = outdir.join("gleam-stdlib.tar");
@@ -11,7 +13,7 @@ pub fn main() {
         assert!(Command::new("git")
             .arg("clone")
             .arg("-b")
-            .arg("fix-quickjs")
+            .arg("bigint")
             .arg("https://github.com/malbarbo/gleam-stdlib")
             .arg(stdlib)
             .status()
