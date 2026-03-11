747a1862e24d3a8b44c2e728e4c09ba56a29bceb
Marco A L Barbosa <malbarbo@gmail.com>
2026-03-11 09:36:36 -0300

    Add deno formatting, improve github workflow.

--- sgleam/sgleam_ffi.mjs
+++ sgleam/sgleam_ffi.mjs
@@ -1,6 +1,15 @@
-import { isEqual, List } from '../gleam.mjs';
-import { inspect } from '../gleam/string.mjs';
-import { rectangle, ellipse, line, crop, beside, text, empty, to_svg } from '../sgleam/image.mjs';
+import { isEqual, List } from "../gleam.mjs";
+import { inspect } from "../gleam/string.mjs";
+import {
+    beside,
+    crop,
+    ellipse,
+    empty,
+    line,
+    rectangle,
+    text,
+    to_svg,
+} from "../sgleam/image.mjs";
 
 export function try_main(main, input_kind, show_output) {
     try {
@@ -31,7 +40,7 @@ function read_lines() {
         if (line == null) {
             return r;
         }
-        r.push(line)
+        r.push(line);
     }
 }
 
@@ -39,18 +48,20 @@ export function run_tests(modules) {
     globalThis.successes = 0;
     globalThis.failures = 0;
     globalThis.errors = 0;
-    console.log('Running tests...');
+    console.log("Running tests...");
     for (let i = 0; i < modules.length; i++) {
-        const module = modules[i]
+        const module = modules[i];
         for (const fname of Object.keys(module)) {
-            if (!fname.endsWith('_examples')) {
+            if (!fname.endsWith("_examples")) {
                 continue;
             }
 
             try {
                 module[fname]();
             } catch (err) {
-                console.log("sgleam internal error: please create a bug report.");
+                console.log(
+                    "sgleam internal error: please create a bug report.",
+                );
                 throw err;
             }
         }
@@ -58,28 +69,32 @@ export function run_tests(modules) {
 
     const { successes, failures, errors } = globalThis;
     const total = successes + failures + errors;
-    console.log(`${total} tests, ${successes} success(es), ${failures} failure(s) and ${errors} error(s).`);
+    console.log(
+        `${total} tests, ${successes} success(es), ${failures} failure(s) and ${errors} error(s).`,
+    );
 }
 
 export function repl_save(value) {
     if (!globalThis.repl_vars) {
-        globalThis.repl_vars = []
+        globalThis.repl_vars = [];
     }
     globalThis.repl_vars.push(value);
     return value;
 }
 
 export function repl_load(index) {
-    return globalThis.repl_vars[index]
+    return globalThis.repl_vars[index];
 }
 
 export function repl_print(value) {
-    if (value && (value.constructor == rectangle(0.0, 0.0).constructor ||
-        value.constructor == ellipse(0.0, 0.0).constructor ||
-        value.constructor == line(0.0, 0.0).constructor || // Polygon
-        value.constructor == beside(empty, empty).constructor || // Combinatin
-        value.constructor == text("", 0.0).constructor ||
-        value.constructor == crop(empty).constructor)) {
+    if (
+        value && (value.constructor == rectangle(0.0, 0.0).constructor ||
+            value.constructor == ellipse(0.0, 0.0).constructor ||
+            value.constructor == line(0.0, 0.0).constructor || // Polygon
+            value.constructor == beside(empty, empty).constructor || // Combinatin
+            value.constructor == text("", 0.0).constructor ||
+            value.constructor == crop(empty).constructor)
+    ) {
         if (sgleam.draw_svg) {
             sgleam.draw_svg(`${to_svg(value)}`);
         } else {
@@ -106,7 +121,7 @@ export function check_equal(a, b, path, function_name, line_number) {
             globalThis.successes += 1;
             return true;
         } else {
-            show_check_failure(a_, b_, null, path, function_name, line_number)
+            show_check_failure(a_, b_, null, path, function_name, line_number);
             globalThis.failures += 1;
             return false;
         }
@@ -118,14 +133,21 @@ export function check_equal(a, b, path, function_name, line_number) {
 }
 
 export function check_true(a, path, function_name, line_number) {
-    check_equal(a, () => true, path, function_name, line_number)
+    check_equal(a, () => true, path, function_name, line_number);
 }
 
 export function check_false(a, path, function_name, line_number) {
-    check_equal(a, () => false, path, function_name, line_number)
+    check_equal(a, () => false, path, function_name, line_number);
 }
 
-export function check_approx(a, b, tolerance, path, function_name, line_number) {
+export function check_approx(
+    a,
+    b,
+    tolerance,
+    path,
+    function_name,
+    line_number,
+) {
     try {
         const a_ = a();
         const b_ = b();
@@ -134,7 +156,14 @@ export function check_approx(a, b, tolerance, path, function_name, line_number) 
             globalThis.successes += 1;
             return true;
         } else {
-            show_check_failure(a_, b_, tolerance_, path, function_name, line_number)
+            show_check_failure(
+                a_,
+                b_,
+                tolerance_,
+                path,
+                function_name,
+                line_number,
+            );
             globalThis.failures += 1;
             return false;
         }
@@ -146,9 +175,9 @@ export function check_approx(a, b, tolerance, path, function_name, line_number) 
 }
 
 function show_check_failure(a, b, tolerance, path, function_name, line_number) {
-    const space = (tolerance !== null) ? ' ' : '';
+    const space = (tolerance !== null) ? " " : "";
     // remove src/
-    const file = path.slice(4)
+    const file = path.slice(4);
     console.log(`Failure at ${location(file, function_name, line_number)}`);
     console.log(`  Actual  ${space}: ${inspect(a)}`);
     console.log(`  Expected${space}: ${inspect(b)}`);
@@ -168,7 +197,7 @@ function show_check_error(err, path, function_name, line_number) {
 }
 
 function location(file, fname, line_number) {
-    if (fname !== '') {
+    if (fname !== "") {
         return `${file} (${fname}:${line_number})`;
     } else {
         return `${file}`;
@@ -178,19 +207,29 @@ function location(file, fname, line_number) {
 function show_error(err) {
     if (err.gleam_error) {
         // remove src/
-        const file = err.file.slice(4)
+        const file = err.file.slice(4);
         console.log(`Error at ${location(file, err.fn, err.line)}`);
         console.log(`  ${err.message}`);
         for (const k in err) {
-            if (!['message', 'gleam_error', 'module', 'file', 'line', 'function', 'fn'].includes(k)) {
+            if (
+                ![
+                    "message",
+                    "gleam_error",
+                    "module",
+                    "file",
+                    "line",
+                    "function",
+                    "fn",
+                ].includes(k)
+            ) {
                 console.log(`  ${k}: ${err[k]}`);
             }
         }
-    } else if (err.message == 'stack overflow') {
-        let stack = err.stack.split('\n');
+    } else if (err.message == "stack overflow") {
+        let stack = err.stack.split("\n");
         stack = stack.slice(-20, -3).reverse();
-        stack.push('    ...');
-        console.log('Stack overflow')
+        stack.push("    ...");
+        console.log("Stack overflow");
         for (const f of stack) {
             console.log(`${f.slice(2)}`);
         }
@@ -235,9 +274,9 @@ export function get_key_event() {
 }
 
 export function text_width(text, font, size) {
-    return sgleam.text_width(text, font, size)
+    return sgleam.text_width(text, font, size);
 }
 
 export function text_height(text, font, size) {
-    return sgleam.text_height(text, font, size)
+    return sgleam.text_height(text, font, size);
 }
